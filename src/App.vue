171ba44d90d25a2ed9cc85e3bc67a015310ca3ab
2gacem7 <gacemcode@gmail.com>
2023-07-06 16:00:27 +0200

    [UI] Implémentation of the ui mars project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="The Mars Files" />
+    <div class="logo-nav">
+      <img alt="Vue logo" src="./assets/logo.png" width="300" />
+      <div class="logo-text">The Mars Files</div>
+    </div>
+    <el-container style="min-height: 800px; border: 1px solid #eee">
+      <el-aside width="300px" style="background-color: #ff9d6f; padding: 12px">
+        <div class="aside-title">CHOOSE YOUR VIEW !</div>
+        <div class="aside-action">
+          <el-button>Curiosity</el-button>
+          <el-button>Opportunity</el-button>
+          <el-button>Spirit</el-button>
+        </div>
+      </el-aside>
+      <el-container>
+        <el-header style="text-align: right; font-size: 12px">
+          <img src="./assets/marsLogo.gif" width="48" />
+        </el-header>
+        <el-main>
+          <ImagesContent />
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import ImagesContent from "./components/ImagesContent.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    ImagesContent,
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap");
+
+.logo-nav {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo-text {
+  font-family: "Bebas Neue", sans-serif;
+  font-size: 80px;
+  color: #000;
+}
+
+.aside-title {
+  font-family: "Bebas Neue", sans-serif;
+  font-size: 64px;
+}
+
+.aside-action {
+  display: flex;
+}
+
+button {
+  font-family: "Bebas Neue", sans-serif;
 }
 </style>
